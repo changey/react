623192a7a5d2cff6d0d6b75f5e2859b636c92c85
Eric Chang <echang@sunrunhome.com>
2015-02-13 14:29:53 -0800

    added the react sample

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,4 +1,33 @@
-React.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('example')
+// React.render(
+//   <h1>Hello, world!</h1>,
+//   document.getElementById('example')
+// );
+
+var Picture = React.createClass({
+	render: function() {
+		return (
+
+	            <div>
+	                <h1>Popular Instagram pics</h1>
+	            </div>
+
+	        );
+	}
+});
+
+var PictureList = React.createClass({
+    
+    getInitialState: function() {
+
+    	return { pictures: [], favorites: [] };
+    },
+
+    render: function() {
+    	
+    }
+});
+
+React.renderComponent(
+    <Picture />,
+    document.body
 );
